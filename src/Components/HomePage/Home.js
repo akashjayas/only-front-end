6ae029ccfc9d67a9fc29dd9cf90bffb19d637aea
Akash Kumar Jayas <akash.jayas_cs.aiml19@gla.ac.in>
2024-01-08 01:48:13 +0530

    first commit

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,62 +1,66 @@
-import React,{useState,useEffect} from 'react'
-import NavBar from '../NavBar/NavBar'
-import vid from './pexels_videos_2096549 (1080p).mp4'
-import './Home.css'
-import EventComponent from '../Events/EventComponent'
-import TouristSpotComponent from '../TouristSpots/TouristSpotComponent'
-import Event_Details from '../Files/Event_Details'
-import Tourist_Spot_Details from '../Files/TouristSpotDetails'
+import React, { useState, useEffect } from "react";
+import NavBar from "../NavBar/NavBar";
+import vid from "./pexels_videos_2096549 (1080p).mp4";
+import "./Home.css";
+import EventComponent from "../Events/EventComponent";
+import TouristSpotComponent from "../TouristSpots/TouristSpotComponent";
+import Event_Details from "../Files/Event_Details";
+import Tourist_Spot_Details from "../Files/TouristSpotDetails";
+
 function Home() {
-  
-  const [firstEvent,setFirstEvent]=useState();
-  const noOfEvents=Event_Details.length;
+  const [firstEvent, setFirstEvent] = useState();
+  const noOfEvents = Event_Details.length;
 
   return (
-    <div className='home' >
-      <video src={vid} autoPlay loop muted/>
-      <div className='header'>
-        <div className='nav-container'><NavBar/></div>
-        <div className='header-content'>welcome to trip partner</div>
-      </div>
-      <div className='body'><div className='container'>
-        <h3>Events</h3>
-      </div>
-          <div className='popular-event-container'>
-              {
-              Event_Details.map
-                ((details,index)=>
-                  (
-                    <EventComponent
-                    eventId={details.event_id}
-                    eventName={details.event_name} 
-                    eventDiscription={details.event_discription} 
-                    eventDate={details.event_happening} 
-                    eventImage={details.event_image} 
-                    eventAddress={details.event_address} 
-                    alt={details.image_alt}/>
-                  )
-                )
-              }
-          </div>
-          <div className='container'><h3>Popular Spots</h3></div>
-          <div className='popular-hotspot-container'>
-            {Tourist_Spot_Details.map(spot=>(
+    <div className="home">
+      <video src={vid} autoPlay loop muted />
+      <div className="header">
+        <div className="nav-container">
+          {" "}
+          <NavBar />{" "}
+        </div>{" "}
+        <div className="header-content"> welcome to trip partner </div>{" "}
+      </div>{" "}
+      <div className="body">
+        {" "}
+        <div className="container">
+          <h3> Events </h3>{" "}
+        </div>{" "}
+        <div className="popular-event-container">
+          {" "}
+          {Event_Details.map((details, index) => (
+            <EventComponent
+              eventId={details.event_id}
+              eventName={details.event_name}
+              eventDiscription={details.event_discription}
+              eventDate={details.event_happening}
+              eventImage={details.event_image}
+              eventAddress={details.event_address}
+              alt={details.image_alt}
+            />
+          ))}{" "}
+        </div>{" "}
+        <div className="container">
+          {" "}
+          <h3> Popular HotSpots </h3>
+        </div>
+        <div className="popular-hotspot-container">
+          {" "}
+          {Tourist_Spot_Details.map((spot) => (
             <TouristSpotComponent
-            spotId={spot.spot_id}
-            spotName={spot.spot_name}
-            spotDescription={spot.spot_description}
-            spotImage={spot.spot_image}
-            spotAlt={spot.image_alt}/>
-            ))}
-          </div>
-          </div>
-          <div className='footer'>
-              <div className='footer-container'>
-                hello
-              </div>
-          </div>
-      
-  </div>
-  )
+              spotId={spot.spot_id}
+              spotName={spot.spot_name}
+              spotDescription={spot.spot_description}
+              spotImage={spot.spot_image}
+              spotAlt={spot.image_alt}
+            />
+          ))}{" "}
+        </div>{" "}
+      </div>{" "}
+      <div className="footer">
+        <div className="footer-container"> </div>{" "}
+      </div>
+    </div>
+  );
 }
-export default Home
+export default Home;
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,53 +1,73 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
-import '../styleguide.css';
-import LoginPage from '../LoginPage/LoginPage';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import "../styleguide.css";
+import LoginPage from "../LoginPage/LoginPage";
 
-import SearchBar from '../SearchBar/SearchBar'
+import SearchBar from "../SearchBar/SearchBar";
+
 function NavBar() {
   const [visible, setvisible] = useState(false);
-  const [profileAva,setProfileAva]=useState("https://trip-partner.s3.eu-north-1.amazonaws.com/login_signUp.svg");
+  const [profileAva, setProfileAva] = useState(
+    "https://trip-partner.s3.eu-north-1.amazonaws.com/login_signUp.svg"
+  );
   const login = () => {
     setvisible(!visible);
   };
-  const childValue=(value)=>{
+  const childValue = (value) => {
     setProfileAva(value);
     console.log(value);
   };
   return (
-    <div className='nav-bar'>
-      <div className='Trip-Logo-Container' style={{display:'flex',alignItems:'center'}}>
+    <div className="nav-bar">
+      <div
+        className="Trip-Logo-Container"
+        style={{ display: "flex", alignItems: "center" }}
+      >
         <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-          <img className='Trip-Logo' src="https://trip-partner.s3.eu-north-1.amazonaws.com/Logo.png"/>
-        </Link>
-      </div>
-      <div className='menu-list' >
-        <div className='nav'>
+          <img
+            className="Trip-Logo"
+            src="https://trip-partner.s3.eu-north-1.amazonaws.com/Logo.png"
+          />
+        </Link>{" "}
+      </div>{" "}
+      <div className="menu-list">
+        <div className="nav">
           <Link to="/EventsHome">
-            <button>Events</button>
-          </Link>
-        </div>
-        <div className='nav'>
+            <button> Events </button>{" "}
+          </Link>{" "}
+        </div>{" "}
+        <div className="nav">
           <Link to="/HotSpotsHome">
-            <button>Hot Spots</button>
-          </Link>
-        </div>
-        <div className='nav'>
+            <button> Hot Spots </button>{" "}
+          </Link>{" "}
+        </div>{" "}
+        <div className="nav">
           <Link to="/OrganizerHome">
-            <button>Organizers</button>
-          </Link>
-        </div>
-      </div>
-      <div className='search-container' >
-          <SearchBar/>
-        </div>
-      <div className='login-signup-outline'>
-        <button className='login-button' onClick={login}>
-          <img className='login-signup' alt='login or sign up' src={profileAva} />
-        </button>
-      </div>
-      {visible && <LoginPage onClose={() => {setvisible(false)}} onReturn={childValue}/>}
+            <button> Organizers </button>{" "}
+          </Link>{" "}
+        </div>{" "}
+      </div>{" "}
+      <div className="search-container">
+        <SearchBar />
+      </div>{" "}
+      <div className="login-signup-outline">
+        <button className="login-button" onClick={login}>
+          <img
+            className="login-signup"
+            alt="login or sign up"
+            src={profileAva}
+          />{" "}
+        </button>{" "}
+      </div>{" "}
+      {visible && (
+        <LoginPage
+          onClose={() => {
+            setvisible(false);
+          }}
+          onReturn={childValue}
+        />
+      )}{" "}
     </div>
   );
 }
